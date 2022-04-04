5caadd968f80887ec716424b6518cb4c0bdbddca
fayeyoussou <fayeyousso@gmail.com>
2022-04-04 04:22:51 +0000

    01-starting/02-dynamic-data

--- 01-starting/src/components/ExpenseItem.js
+++ 01-starting/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    const expenseDate = new Date(2021,4,4);
+    const expenseTitle = "tandarma"
+    const expenseAmount = 1200
+    return (<div className="expense-item">
+        <div >{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2 >{expenseTitle}</h2>
+            <div className = "expense-item__price ">{expenseAmount} F </div>
+        </div>
+    </div>)
 }
 
 export default ExpenseItem;
