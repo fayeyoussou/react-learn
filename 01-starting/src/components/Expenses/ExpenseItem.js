f539277ce400844ef64bcafadedecb82f1a31a78
fayeyoussou <fayeyousso@gmail.com>
2022-04-04 22:08:10 +0000

    02-state-event/01-useStaten

--- 01-starting/src/components/Expenses/ExpenseItem.js
+++ 01-starting/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import ExpenseDate from "./ExpenseDate";
+import { useState }  from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-const ExpenseItem = (props) => (
+const ExpenseItem = (props) => {
+    const [title,setTitle]=useState(
+        props.expense.title
+    ) 
+    const clickHandler = ()=> {
+        setTitle("Youssou")
+    }
+        return (
   <Card className="expense-item">
     <div className="expense-item__description">
       <ExpenseDate datb={props.expense.datb} />
-      <h2>{props.expense.title}</h2>
+      <h2>{title}</h2>
       <div className="expense-item__price ">{props.expense.amount} F </div>
     </div>
+    <button onClick={clickHandler}>change title</button>
   </Card>
-);
+)}
 
 export default ExpenseItem;
