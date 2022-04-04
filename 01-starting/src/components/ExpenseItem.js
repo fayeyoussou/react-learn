14b886b3d190196d803ce583746f718675f7bdb5
fayeyoussou <fayeyousso@gmail.com>
2022-04-04 12:09:20 +0000

    01-starting/05-Components

--- 01-starting/src/components/ExpenseItem.js
+++ 01-starting/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from './Card';
 function ExpenseItem(props) {
     
 
-    return (<div className="expense-item">
+    return (<Card className="expense-item">
         
         <div className="expense-item__description">
             <ExpenseDate datb = {props.expense.datb}/>
             <h2 >{props.expense.title}</h2>
             <div className = "expense-item__price ">{props.expense.amount} F </div>
         </div>
-    </div>)
+    </Card>)
 }
 
 export default ExpenseItem;
