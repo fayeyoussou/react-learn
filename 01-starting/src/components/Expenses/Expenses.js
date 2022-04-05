f3658b76e18187d5571925cbeea7f65b92154390
fayeyoussou <fayeyousso@gmail.com>
2022-04-05 12:26:02 +0000

    03-List/02-conditionnal-return

--- 01-starting/src/components/Expenses/Expenses.js
+++ 01-starting/src/components/Expenses/Expenses.js
@@ -1,35 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpenseList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
+  let res = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <div>
-      <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {
-          props.items.filter(item=>{
-            
-            return item.date.getFullYear() == filteredYear
-          }).map(item=>{
-            return <ExpenseItem
-              key = {item.id}
-              title = {item.title}
-              amount = {item.amount}
-              date = {item.date}
-            />
-          })
-        }
-        
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseList expenses={res} />
       </Card>
     </div>
   );
